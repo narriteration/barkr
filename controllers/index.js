980f6b9671256aab39592c0fbf99053185b335db
Jesser360 <jesser360@gmail.com>
2017-03-13 15:44:21 -0700

    package.json

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  api: require('./apiController'),
+  dog: require('./dogsController'),
+  owner: require('/ownersController')
+};
---
+++ models/owner.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+  var OwnerSchema = new Schema({
+    ownerName:String,
+    //isSingle: Boolean,
+    gender:String,
+    age:Number,
+    imgOwner:String,
+    email:String
+  })
+
+  var Owner = mongoose.model('Owner', OwnerSchema);
+
+  module.exports = Owner;

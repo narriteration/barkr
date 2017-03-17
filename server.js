6171c9bd70f9ed7cb7384863c4bfcf866d1626ea
narriteration <narriteration@gmail.com>
2017-03-16 18:19:28 -0700

    fixed syntax error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ session = require('express-session');
 
 var app = express();
 var db = require('./models');
-+var Owner = db.Owner;
+var Owner = db.Owner;
 
 var controllers = require('./controllers');
 

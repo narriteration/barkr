9696bd57e6980e2d21755cc918ae55cc0acfab30
narriteration <narriteration@gmail.com>
2017-03-17 17:03:25 -0700

    fixed little error in index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
   var mongoose = require('mongoose');
   mongoose.Promise = global.Promise;
 
-  mongolab-slippery-43458
   mongoose.connect(process.env.mongolab-slippery-43458 || "mongodb://localhost/barkr")
 
   module.exports.Dog = require('./dog.js')

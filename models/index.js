d30b8d84d9de8af64c341f8cb608dc29436057fa
narriteration <narriteration@gmail.com>
2017-03-26 18:51:09 -0700

    re-added mongoose.connect line: NOT WORKING

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
   var mongoose = require('mongoose');
   mongoose.Promise = global.Promise;
 
-  mongoose.connect(process.env.mongolab-slippery-43458 || "mongodb://localhost/barkr")
+  // mongoose.connect(process.env.mongolab-slippery-43458 || "mongodb://localhost/barkr")
+
+
+  // mongoose.connect( process.env.MONGODB || "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
+
 
   module.exports.Dog = require('./dog.js')
   module.exports.Owner = require('./owner.js')
